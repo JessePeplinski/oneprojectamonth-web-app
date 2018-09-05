db9ead6d4eceb33df1bf2169984b4e213bcb49f7
Jesse Peplinski <peplinski.jesse@gmail.com>
2018-09-04 21:31:10 -0400

    Remove map error

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -29,7 +29,7 @@ const ROUTES = [
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClient,
+    HttpClientModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [PostsService],
--- angular/src/app/services/posts.service.ts
+++ angular/src/app/services/posts.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
+
+const url = 'https://jsonplaceholder.typicode.com';
+const url2 = 'http://localhost:8080/api/posts';
 
 @Injectable()
 export class PostsService {
 
-  constructor(private http: HttpClientModule) { }
+  constructor(private http: HttpClient) { }
 
   // Get all posts from the API
   getAllPosts() {
-    return this.http.get('http://localhost:8080/api/posts').map(res => res.json());
+    return this.http.get(url).map((res: Response) => res.json());
   }
 }

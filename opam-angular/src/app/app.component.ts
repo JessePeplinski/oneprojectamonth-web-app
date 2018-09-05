ed81ac511305a49a76be24ac96bec9af8c5c74c8
Jesse Peplinski <peplinski.jesse@gmail.com>
2018-09-05 19:19:03 -0400

    Announcement!

--- opam-angular/src/app/app.component.ts
+++ opam-angular/src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
-import { AngularFirestore } from 'angularfire2/firestore';
+import { Component, Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
-export class AppComponent {
-  items: Observable<any[]>;
-  constructor(db: AngularFirestore) {
-    this.items = db.collection('items').valueChanges();
+
+export class AppComponent implements OnInit {
+
+  announcementsCollection: AngularFirestoreCollection<any[]>;
+  announcements: Observable<any[]>;
+
+  constructor(private afs: AngularFirestore) { }
+
+  ngOnInit() {
+    this.announcementsCollection = this.afs.collection('announcements', ref => {
+      return ref;
+    });
+    this.announcements = this.announcementsCollection.valueChanges();
   }
 }

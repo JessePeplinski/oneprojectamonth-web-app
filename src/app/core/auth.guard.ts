93df31867e19fb7714eb6902f2078ea08bed5237
Jonathan Fox <fox.jonathan77@gmail.com>
2018-10-23 21:27:53 -0400

    Fix browser overload after login

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -19,6 +19,9 @@ export class AuthGuard implements CanActivate {
         if (!loggedIn) {
           console.log('access denied');
           this.router.navigate(['/sign-up']);
+          return false;
+        } else {
+          return true;
         }
       })
     );
